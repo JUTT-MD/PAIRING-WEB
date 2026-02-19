bdcb76a518c41fea191e79fd10dd398d83924fae
JUTT-MD <fazijutt8990@gmail.com>
2026-02-20 00:20:20 +0500

    Change custom URL format in Paste.js

--- Paste.js
+++ Paste.js
@@ -57,7 +57,7 @@ async function uploadToPastebin(input, title = 'Untitled', format = 'json', priv
         }
 
         const pasteId = pasteUrl.replace(/https?:\/\/[^/]+\//, '');
-        const customUrl = `GlobalTechInfo/MEGA-MD_${pasteId}`;
+        const customUrl = `Faizan-MD-BOTZ/FAIZAN-XMD_${pasteId}`;
 
         console.log('✅ Session paste URL:', customUrl);
         return customUrl;
